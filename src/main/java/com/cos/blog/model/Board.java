6fdd97c767b34fd58bcc386d4a5601655d96819b
hskoo <nrudev812@gmail.com>
2021-06-30 13:34:40 +0900

    create table practice with jpa

---
+++ src/main/java/com/cos/blog/model/Board.java
@@ -0,0 +1,39 @@
+package com.cos.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob// 대용량 데이터
+    private String content; // 섬머노트 라이브러리 <html> 태그가 섞여서 디자인됨. => 글자 용량 커짐
+
+    @ColumnDefault("0")
+    private int count; // 조회수
+
+    @ManyToOne // Many = Board, User = One 한 명의 유저는 여러 개의 게시글을 쓸 수 있다.
+    @JoinColumn(name = "userId")
+    private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
---
+++ src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,38 @@
+package com.cos.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // 빌더 패턴
+@Entity // User 클래스가 MariaDB에 테이블이 생성된다.
+public class User {
+
+    @Id // Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다. => auto_increment
+    private Long id; // auto_increment(오라클의 sequence)
+
+    @Column(nullable = false, length = 30)
+    private String username; // 아이디
+
+    @Column(nullable = false, length = 100) // 왜 이렇게 길게? 123456 => 해쉬(비밀번호 암호화)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @ColumnDefault("'user'")
+    private String role; // 타입은 Enum을 쓰는게 좋다. Enum을 쓰면 데이터의 도메인을 만들어줄 수 있다. => 오타 방지, 범위 설정
+
+    @CreationTimestamp // 시간이 자동 입력
+    private Timestamp createDate;
+}
