cb93e07fc6a907fef58e597e2e094c31d6da50d2
hskoo <nrudev812@gmail.com>
2021-07-02 18:01:00 +0900

    게시글 목록 조회
    JpaRepository의 메서드 findAll() 을 통해 손쉽게 테이블에 있는 모든 게시물을 불러올 수 있음.
    view에서는 jstl을 이용해 가져온 데이터를 간단하게 뿌려줄 수 있음.

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,16 +1,26 @@
 package com.cos.blog.controller;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
+    // (@AuthenticationPrincipal PrincipalDetail principal) 컨트롤러에서 세션 찾기
     @GetMapping({"", "/"})
-    public String index() { // @AuthenticationPrincipal PrincipalDetail principal 컨트롤러에서 세션 찾기
-        return "index"; // /WEB-INF/views/index.jsp
+    public String index(Model model) { // Model : 다른 페이지로 데이터를 가져갈 때 필요
+        model.addAttribute("boards", boardService.글목록());
+        return "index";
+        /* @Controller는 리턴할 때 viewResolver 작동.
+        리턴값에 prefix, suffix 붙여주고, 해당 페이지로 model의 정보를 들고 이동. */
     }
 
     @GetMapping("/board/saveForm")
